2dc3c55d5525e950bb037ae8f6010edd7efae39b
Kate Martin <kate@renanthera.lgbt>
2019-11-21 12:15:35 -0700

    Finished up parsing data input.

--- data/combine_data.py
+++ data/combine_data.py
@@ -8,11 +8,19 @@ drugOD = drugOD.drop('State',axis=1)
 drugSales = pd.read_csv(r'total_data.csv',header=0,usecols=['BUYER_STATE','BUYER_COUNTY','YEAR','QUANTITY','DOSAGE_UNIT'])
 drugSales['County'] = drugSales['BUYER_COUNTY'] + drugSales['BUYER_STATE']
 drugSales = drugSales.drop(['BUYER_COUNTY','BUYER_STATE'],axis=1)
+drugSales = drugSales.rename(columns={'YEAR':'Year','QUANTITY':'Quantity','DOSAGE_UNIT':'Dosage Unit'})
 
 weather = pd.read_csv(r'NALDAS_DAILY-MAX-AIR-TEMP-AVG.tsv',sep='\t',header=0,usecols=['Notes','County','County Code','Year','Year Code','Avg Daily Max Air Temperature (F)'])
-for index,row in weather.iterrows():
-        row['County'] = row['County'][-2:] + row['County'].split(',')[0]
+weather['County'] = (weather['County'].str[:-11] + weather['County'].str[-2:]).str.upper()
+weather = weather.drop(weather.tail(18).index)
+weather = weather.drop(['County Code','Year Code'],axis=1)
+weather = weather.rename(columns={'Avg Daily Max Air Temperature (F)':'Temp'})
+weatherTotal = weather[weather['Notes'] == 'Total']
+weather = weather.drop('Notes',axis=1)
+weather = weather.dropna()
+weatherTotal = weatherTotal.drop(['Notes','Year'],axis=1)
 
 print(drugOD)
 print(drugSales)
 print(weather)
+print(weatherTotal)
