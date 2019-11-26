159c1555ca9351a3f125746388feebd6b6f75788
Kate Martin <kate@renanthera.lgbt>
2019-11-26 14:15:17 -0700

    Changes to be committed: 	modified:   add_states.py 	modified:   county_fips_master.csv 	modified:   ../topojson-counties.json Fixed a bug in add_states and added several features

--- data/datasets/add_states.py
+++ data/datasets/add_states.py
@@ -4,27 +4,31 @@ import re
 def insert(string, index, value):
     return string[:index] + value + string[index:]
 
-fips = pd.read_csv(r'county_fips_master.csv',header=0,usecols=['fips','state_abbr'])
+fips = pd.read_csv(r'county_fips_master.csv',header=0,usecols=['fips','state_abbr','county_name','state_name'],encoding='iso-8859-1')
+fips['id'] = (fips['county_name'].str.split(' ').str[:-1].str.join('')+fips['state_abbr']).str.upper()
 topo = open('topojson-counties.json','r+')
 topo = topo.read()
-#print(type(topo))
 
 #print(fips)
 
-regex = '"id":"(\d{5})","properties":{"name":"[A-Z][a-z]*(")}'
+regex = '"id":"(\d{5})","properties":{"name":"([A-z\s]*)(")}'
 
 toposplit = re.finditer(regex,topo)
 
 k = 0
 for i in toposplit:
-    a = i.span(1)
-    b = i.span(2)
-    fipscode = i.groups()[0]
-#    print(k,i.group(),i.span(0),i.span(1))
-#    print(" ",topo[a[0]:a[1]])
-#    print(" ",i.groups())
-    state = ',"state":"'+fips[fips['fips'] == int(fipscode)].values[0][1]+'"'
-    topo = insert(topo, b[1]+k*len(state), state)
+    fipscode = int(i.groups()[0])
+    state = ',"state":"'+fips[fips['fips'] == fipscode].values[0][3]+'"'
+    statecode = ',"state_short":"'+fips[fips['fips'] == fipscode].values[0][2]+'"'
+    ident = ',"id":"'+fips[fips['fips'] == fipscode].values[0][4]+'"'
+    string = ''.join([state,statecode,ident])
+#    print("id:",fipscode)
+#    print(" i:",i.group())
+#    print(" groups:",i.groups())
+#    print(" newstring:",string)
+    j = re.search(i.group(),topo)
+#    print(" j:",j.group())
+    topo = insert(topo, j.span(0)[1]-1, string)
     k += 1
 
 print(topo)
