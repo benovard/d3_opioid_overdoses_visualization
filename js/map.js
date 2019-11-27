34043f0ee83dffc40af5b742d34bb3f6c60537c7
Kate Martin <kate@renanthera.lgbt>
2019-11-27 16:36:04 -0700

    I haven't managed to nuke it yet in my refactor, so that's good

--- js/map.js
+++ js/map.js
@@ -1,28 +1,13 @@
-class Map{
-
-    constructor(year, data){
-        this.width = 960; 
-        this.height = 750;
-        this.svg = d3.select("body").select("#map")
-            .attr("width", this.width)
-            .attr("height", this.height)
-            ;
-        this.map = this.svg.append("g")
-            .attr("class", "map")
-            ;
-        this.year = String(year);
-        this.selectedData = data;
-        this.mapData = {};
-        this.features;
+class Map {
+    constructor(year, data) {
         this.us;
         this.data;
-
         d3.queue()
-            .defer(d3.json, "data/topojson-counties.json")
-            .defer(d3.json, "data/data.json")
+            .defer(d3.json, 'data/topojson-counties.json')
+            .defer(d3.json, 'data/data.json')
             .await((error, us, data) => {
                 if (error) {
-                    console.log("Uh oh: " + error);
+                    console.log('Uh oh: ' + error);
                 }
                 else {
                     this.us = us;
@@ -30,20 +15,35 @@ class Map{
                     this.drawMap()
                 }
             });
+
+        this.width = 960; 
+        this.height = 750;
+        this.svg = d3.select('body').select('#map')
+            .attr('width', this.width)
+            .attr('height', this.height);
+
+        this.map = this.svg.append('g')
+            .attr('class', 'map');
+
+        this.year = String(year);
+        this.selectedData = data;
+        this.mapData = {};
+
+        this.path = d3.geoPath();
     }
 
     drawMap() {
-
-        var path = d3.geoPath();
-        this.colorScale = d3.scaleThreshold()
-            .domain([1, 10, 20, 30, 40, 50, 60, 70, 80, 90])
-            .range(["#f7fcfd", "#e0ecf4", "#bfd3e6", "#9ebcda", "#8c96c6", "#8c6bb1", "#88419d", "#810f7c", "#4d004b"])
-            ;
-        
+        const ls_w = 96; // width of the map divided by 10 gives us 10 cells with a size of 96
+        const ls_h = 20; // height of a cell for the legend
         var ext_color_domain = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90];
-        var legend_labels = ["< 1", "10+", "20+", "30+", "40+", "50+", "60+", "70+", "80+", "90+"];
+        var ext_color_range = ['#f7fcfd', '#e0ecf4', '#bfd3e6', '#9ebcda', '#8c96c6', '#8c6bb1', '#88419d', '#810f7c', '#4d004b']
+        var legend_labels = ['< 1', '10+', '20+', '30+', '40+', '50+', '60+', '70+', '80+', '90+'];
 
         this.features = topojson.feature(this.us, this.us.objects.counties).features;
+
+        this.colorScale = d3.scaleThreshold()
+            .domain(ext_color_domain)
+            .range(ext_color_range);
 
         // create mapData from data
         for (var key in this.data){
@@ -66,58 +66,55 @@ class Map{
 
         console.log(this.features);
 
-        this.map.append("g")
-            .attr("class", "counties")
-            .selectAll("path")
+        this.map.append('g')
+            .attr('class', 'counties')
+            .selectAll('path')
             .data(this.features)
             .enter()
-            .append("path")
-            .attr("d", path)
-            .attr("id", function (d) {
+            .append('path')
+            .attr('d', this.path)
+            .attr('id', function (d) {
                 return d.properties.id;
             })
-            .style("fill", (d) => {
+            .style('fill', (d) => {
                 return d.details[this.year] && d.details[this.year].quantity ? this.colorScale(d.details[this.year].quantity) : undefined;
             })
             .on('click', function (d) {
                 d3.select(this)
-                    .style("stroke", "white")
-                    .style("stroke-width", 1)
-                    .style("cursor", "pointer")
+                    .style('stroke', 'white')
+                    .style('stroke-width', 1)
+                    .style('cursor', 'pointer')
                     ;
-                d3.select(".county")
-                    .text(d.properties.long_name + ", " + d.properties.state)
+                d3.select('.county')
+                    .text(d.properties.long_name + ', ' + d.properties.state)
                     ;
-                d3.select(".deaths")
-                    .text(d.details && d.details.deaths && "Deaths: " + d.details.deaths)
+                d3.select('.deaths')
+                    .text(d.details && d.details.deaths && 'Deaths: ' + d.details.deaths)
                     ;
-                d3.select(".population")
-                    .text(d.details && d.details.population && "Population: " + d.details.population)
+                d3.select('.population')
+                    .text(d.details && d.details.population && 'Population: ' + d.details.population)
                     ;
                 d3.select('.details')
-                    .style('visibility', "visible")
+                    .style('visibility', 'visible')
                     ;
             })
             .on('mouseout', function (d) {
                 d3.select(this)
-                    .style("stroke", null)
-                    .style("stroke-width", 0.25)
+                    .style('stroke', null)
+                    .style('stroke-width', 0.25)
                     ;
                 d3.select('.details')
-                    .style('visibility', "hidden")
+                    .style('visibility', 'hidden')
                     ;
             })
             ;
 
-        this.map.append("path")
-            .attr("class", "county-borders")
-            .attr("d", path(topojson.mesh(this.us, this.us.objects.counties, function(a, b) { 
+        this.map.append('path')
+            .attr('class', 'county-borders')
+            .attr('d', this.path(topojson.mesh(this.us, this.us.objects.counties, function(a, b) { 
                 return a !== b; 
             })))
         ;
-
-        const ls_w = 96; // width of the map divided by 10 gives us 10 cells with a size of 96
-        const ls_h = 20; // height of a cell for the legend
 
         var legend = this.svg.append('g').attr('class','legend');
 
@@ -135,13 +132,13 @@ class Map{
 		legend.selectAll('text')
             .data(ext_color_domain)
             .enter()
-            .append("text")
-            .attr("x", function(d, i){ return 960 - (i*ls_w) - ls_w;})
-            .attr("y", 720)
+            .append('text')
+            .attr('x', function(d, i){ return 960 - (i*ls_w) - ls_w;})
+            .attr('y', 720)
             .text(function(d, i){ return legend_labels[i]; })
         ;
 
-		var legend_title = "Number of deaths:";
+		var legend_title = 'Number of deaths:';
         legend.append('text')
             .attr('x',0)
             .attr('y',670)
@@ -157,7 +154,7 @@ class Map{
 
         this.map.select('.counties')
             .selectAll('path')
-            .style("fill", (d) => {
+            .style('fill', (d) => {
                 return d.details[this.year] && d.details[this.year].quantity ? this.colorScale(d.details[this.year].quantity) : undefined;
             })
             ;
