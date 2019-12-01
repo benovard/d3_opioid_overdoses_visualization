0b1019ac50be8ce6b96c169265a33635e449e9fc
Kate Martin <kate@renanthera.lgbt>
2019-12-01 16:43:05 -0700

    Mostly reworked combine_data.py

--- data/datasets/combine_data.py
+++ data/datasets/combine_data.py
@@ -2,116 +2,122 @@ import pandas as pd
 
 # this is some of the worst python I have ever written...
 
+def avg(lst):
+    count = 0
+    val = 0
+    for k in lst:
+        if str(k) != 'nan':
+            count += 1
+            val += k
+    if count > 0:
+        return val / count
+    else:
+        return float('nan')
+
 drugOD = pd.read_csv(r'drug_overdoses_2006-2016_cleaned.csv',header=0,usecols=['Year','State','County','Population','Deaths','Death Rate per 100k'])
-drugOD['County'] = (drugOD['County'] + drugOD['State']).str.upper()
-drugOD = drugOD.drop('State',axis=1)
+drugOD['Index'] = (drugOD['County'] + drugOD['State']).str.upper()
+drugOD = drugOD.drop(['State','County'],axis=1)
+drugOD = drugOD[drugOD['Year'] < 2012]
 
 drugSales = pd.read_csv(r'total_data.csv',header=0,usecols=['BUYER_STATE','BUYER_COUNTY','YEAR','QUANTITY','DOSAGE_UNIT'])
-drugSales['County'] = drugSales['BUYER_COUNTY'] + drugSales['BUYER_STATE']
+drugSales['Index'] = drugSales['BUYER_COUNTY'] + drugSales['BUYER_STATE']
 drugSales = drugSales.drop(['BUYER_COUNTY','BUYER_STATE'],axis=1)
 drugSales = drugSales.rename(columns={'YEAR':'Year','QUANTITY':'Quantity','DOSAGE_UNIT':'Dosage Unit'})
 
 weather = pd.read_csv(r'NALDAS_DAILY-MAX-AIR-TEMP-AVG.tsv',sep='\t',header=0,usecols=['Notes','County','County Code','Year','Year Code','Avg Daily Max Air Temperature (F)'])
-weather['County'] = (weather['County'].str.split(',').str[0].str.split(' ').str[:-1].str.join('') + weather['County'].str[-2:]).str.upper()
+weather['Index'] = (weather['County'].str.split(',').str[0].str.split(' ').str[:-1].str.join('') + weather['County'].str[-2:]).str.upper()
 weather = weather.drop(weather.tail(18).index)
 weather = weather.drop('Year Code',axis=1)
 weather = weather.rename(columns={'Avg Daily Max Air Temperature (F)':'Temp'})
 weatherTotal = weather[weather['Notes'] == 'Total']
-weather = weather.drop('Notes',axis=1)
+weather = weather.drop(['Notes','County','County Code'],axis=1)
 weather = weather.dropna()
 weatherTotal = weatherTotal.drop(['Notes','Year'],axis=1)
 
-#print(drugOD)
-#print(drugSales)
-#print(weather)
-#print(weatherTotal)
+# print(drugOD)
+# print(drugSales)
+# print(weather)
+# print(weatherTotal)
 
-dODCounties = drugOD['County'].tolist()
-dSCounties = drugSales['County'].tolist()
-wCounties = weather['County'].tolist()
-counties = set().union(dODCounties, dSCounties, wCounties)
-k = 0
+allCounties = pd.read_csv(r'county_fips_master.csv',header=0,usecols=['fips','county_name','state_name','state_abbr'],encoding='iso-8859-1')
+temp = [(k[0],k[1],k[2],k[3],y) for k in allCounties[['fips','county_name','state_abbr','state_name']].values for y in range(2006,2012)]
+combinedDF = pd.DataFrame(temp,columns=['FIPS','County','Abbr','State','Year'])
+combinedDF['Pretty Name'] = combinedDF['County'] + ', ' + combinedDF['State']
+combinedDF['Index'] = (combinedDF['County'].str.split(' ').str[:-1].str.join('') + combinedDF['Abbr']).str.upper()
+combinedDF = combinedDF.merge(drugOD,how='outer',on=['Index','Year'])
+combinedDF = combinedDF.merge(drugSales,how='outer',on=['Index','Year'])
+combinedDF = combinedDF.merge(weather,how='outer',on=['Index','Year'])
+cols = combinedDF.columns.tolist()
+cols = ['FIPS','Index','County','State','Abbr','Pretty Name','Year','Population','Deaths','Death Rate per 100k','Quantity','Temp']
+combinedDF = combinedDF[cols]
+combinedDF = combinedDF.dropna(subset=['FIPS'])
+combinedDF['FIPS'] = combinedDF['FIPS'].map(str).str[:-2]
+# print(combinedDF)
+
+dODCounties = drugOD['Index'].tolist()
+dSCounties = drugSales['Index'].tolist()
+wCounties = weather['Index'].tolist()
+aCounties = combinedDF['Index'].tolist()
+counties = set().union(dODCounties, dSCounties, wCounties, aCounties)
+
 print('{')
 for i in counties:
-    a = drugOD[drugOD['County'] == i]
-    b = drugSales[drugSales['County'] == i]
-    c = weather[weather['County'] == i]
-    d = weatherTotal[weatherTotal['County'] == i]
-    P, OD, Q, DU, OD100 = 0, 0, 0, 0, 0
-    Pq, ODq, Qq, DUq, OD100q = 0, 0, 0, 0, 0
+    county = combinedDF[combinedDF['Index'] == i]
+    temp = county['FIPS'].tolist()
+    drugoverdoses, overdoses100k, sales, temperature = 0, 0, 0, 0
+    values = [drugoverdoses, overdoses100k, sales, temperature]
+    fips = 'NaN'
+    if len(temp) > 0:
+        fips = temp[0]
+        if len(fips) > 5:
+            fips = '0' + fips
+    if fips == 'NaN':
+        continue
     print('\t"'+i+'" : {')
-    if len(d) > 0:
-        fips = str(int(d.values[0][1]))
-        if len(fips) < 5:
-            fips = '0'+fips
-            kill = 1
-        print('\t\t"fips" : "'+fips+'",')
-    for y in range(2006,2012):
-        print('\t\t"'+str(y)+'" : {')
-        dOD = a[a['Year'] == y].values
-        dS = b[b['Year'] == y].values
-        w = c[c['Year'] == y].values
-        if len(dOD) > 0:
-            print('\t\t\t"Population" : '+str(dOD[0][2])+',')
-            P += dOD[0][2]
-            Pq += 1
-        
-            print('\t\t\t"Drug Overdoses" : '+str(dOD[0][3])+',')
-            OD += dOD[0][3]
-            ODq += 1
-            comma = ''
-            if len(w) > 0 or len(dS) > 0:
-                comma = ','
-            print('\t\t\t"Overdoses per 100k" : '+str(dOD[0][4])+comma)
-            OD100 += dOD[0][4]
-            OD100q += 1
-        
-        if len(dS) > 0:
-            print('\t\t\t"Quantity" : '+str(dS[0][1])+',')
-            Q += dS[0][1]
-            Qq += 1
-            comma = ''
-            if len(w) > 0:
-                comma = ','
-            print('\t\t\t"Dosage Unit" : '+str(dS[0][2])+comma)
-            DU += dS[0][2]
-            DUq += 1
-        if len(w) > 0:
-            print('\t\t\t"Temperature" : '+str(w[0][3]))
+    print('\t\t"fips" : "'+fips+'",')
+    for index,row in county.iterrows():
+        population = row['Population']
+        drugoverdoses = row['Deaths']
+        overdoses100k = row['Death Rate per 100k']
+        sales = row['Quantity']
+        temperature = row['Temp']
+        comma = [',' if str(k) != 'nan' else '' for k in values]
+        print('\t\t"'+str(row['Year'])+'" : {')
+        if str(row['Population']) != 'nan':
+            print('\t\t\t"Population" : '+str(row['Population'])+comma[0])
+        if str(row['Deaths']) != 'nan':
+            print('\t\t\t"Drug Overdoses" : '+str(row['Deaths'])+comma[1])
+        if str(row['Death Rate per 100k']) != 'nan':
+            print('\t\t\t"Overdoses per 100k" : '+str(row['Death Rate per 100k'])+comma[2])
+        if str(row['Quantity']) != 'nan':
+            print('\t\t\t"Quantity" : '+str(row['Quantity'])+comma[3])
+        if str(row['Temp']) != 'nan':
+            print('\t\t\t"Temperature" : '+str(row['Temp']))
         print('\t\t},')
-    if Pq > 0:
-        P /= Pq
-    if ODq > 0:
-        OD /= ODq
-    if Qq > 0:
-        Q /= Qq
-    if DUq > 0:
-        DU /= DUq
+    population = avg(county['Population'].tolist())
+    if str(population) != 'nan': population = round(population)
+    drugoverdoses = avg(county['Deaths'].tolist())
+    if str(drugoverdoses) != 'nan': drugoverdoses = round(drugoverdoses)
+    overdoses100k = avg(county['Death Rate per 100k'].tolist())
+    if str(overdoses100k) != 'nan': overdoses100k = round(overdoses100k)
+    sales = avg(county['Quantity'].tolist())
+    if str(sales) != 'nan': sales = round(sales)
+    temperature = avg(county['Temp'].tolist())
+    comma = [',' if str(k) != 'nan' else '' for k in values]
     print('\t\t"Average" : {')
-    if P != 0:
-        print('\t\t\t"Population" : '+str(round(P))+',')
-    if OD != 0:
-        comma = ''
-        if Q != 0 or len(d.values) > 0:
-            comma = ','
-        print('\t\t\t"Drug Overdoses" : '+str(round(OD))+comma)
-    if Q != 0:
-        print('\t\t\t"Quantity" : '+str(round(Q))+',')
-    comma = ''
-    if len(d.values) > 0:
-        comma = ','
-    if DU != 0:
-        print('\t\t\t"Dosage Unit" :'+str(round(DU))+comma)
-    if len(d.values) > 0:
-        print('\t\t\t"Temperature" : '+str(d.values[0][2]))
+    if str(population) != 'nan':
+        print('\t\t\t"Population" : '+str(population)+comma[0])
+    if str(drugoverdoses) != 'nan':
+        print('\t\t\t"Drug Overdoses" : '+str(drugoverdoses)+comma[1])
+    if str(overdoses100k) != 'nan':
+        print('\t\t\t"Overdoses per 100k" : '+str(overdoses100k)+comma[2])
+    if str(sales) != 'nan':
+        print('\t\t\t"Quantity" : '+str(sales)+comma[3])
+    if str(temperature) != 'nan':
+        print('\t\t\t"Temperature" : '+str(temperature))
     print('\t\t},')
     print('\t\t"Ranking" : {')
-    print('\t\t\t"Population" : ""'+',')
-    print('\t\t\t"Drug Overdoses" : ""'+',')
-    print('\t\t\t"Quantity" : ""'+',')
-    print('\t\t\t"Dosage Unit" : ""'+',')
-    print('\t\t\t"Temperature" : ""')
     print('\t\t}')
     print('\t},')
-    # break
+    break
 print('}')
